05d308e12ffffe0595374b22485a2f22a80a0d32
Kimchretien <kimintsinzi@gmail.com>
2025-12-01 17:12:00 +0200

    update modifier_etudiant.php

--- modifier_etudiant.php
+++ modifier_etudiant.php
@@ -2,18 +2,30 @@
 include 'connexion.php';
 
 $id = $_GET['id'];
+$stmt=$cnx->prepare("SELECT * FROM etudiant WHERE id =?");
+if($stmt ===false){
+    die("Erreur de preparation de la requete" .$cnx->error);
+}
+$stmt->bind_param("i",$id);
 
-$query = "SELECT * FROM etudiant WHERE id = $id";
-$result = mysqli_query($cnx, $query);
-$etudiant = mysqli_fetch_assoc($result);
+if($stmt->execute()){
+    echo"modification reussie avec succes";
+}else{
+    echo "Erreur de l'enregistrement".$cnx->error;
+}
+$result=$stmt->get_result();
+$etudiant=$result->fetch_assoc();
+
+
 ?>
 
-<form method="POST" action="update.php">
-    <input type="hidden" name="id" value="<?php echo $etudiant['id']; ?>">
 
-    Nom : <input type="text" name="nom" value="<?php echo $etudiant['nom']; ?>"><br>
-    Prenom : <input type="text" name="prenom" value="<?php echo $etudiant['prenom']; ?>"><br>
-    Email : <input type="email" name="email" value="<?php echo $etudiant['email']; ?>"><br>
+<form method="POST" action="update.php">
+    <input type="hidden" name="id" value="<?php echo htmlspecialchars($etudiant['id']) ; ?>">
+
+    Nom : <input type="text" name="nom" value="<?php echo htmlspecialchars($etudiant['nom']) ; ?>"><br>
+    Prenom : <input type="text" name="prenom" value="<?php echo htmlspecialchars($etudiant['prenom'])?>"><br>
+    Email : <input type="email" name="email" value="<?php echo htmlspecialchars($etudiant['email'])?>"><br>
 
     <button type="submit">Enregistrer</button>
 </form>
