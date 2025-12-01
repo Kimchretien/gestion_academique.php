5844aed009ca0c2bf8a9554e4a6f183e0d25db12
Kimchretien <kimintsinzi@gmail.com>
2025-12-01 17:09:41 +0200

    update enregistrer_user.php

--- enregistrement_user.php
+++ enregistrement_user.php
@@ -1,21 +1,30 @@
 <?php
 session_start();
 include 'connexion.php';
+
 if(isset($_POST['envoyer'])){
-    $username=$_POST['username'];
-    $password=$_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
+    $stmt = $cnx->prepare("INSERT INTO user(username, password) VALUES (?, ?)");
+    
+    if ($stmt === false) {
+        die("Erreur préparation : " . $cnx->error);
+    }
+    $stmt->bind_param("ss", $username, $password);
 
-    $query="INSERT INTO user(username,password) VALUES('$username','$password')";
-    $result=mysqli_query($cnx,$query);
-
-
-   if($result){
-    echo "Utilisateur creer avec succes";
-    // header("Location: login.html");
-    exit();
-   }else{
-    echo "Erreur lors de l'enregistrement:" .mysqli_error($cnx);
-   }
+    if($stmt->execute()){
+        echo "Utilisateur créé avec succès";
+        // header("Location: login.html");
+        exit();
+    } else {
+        echo "Erreur lors de l'enregistrement : " . $stmt->error;
+    }
+    $stmt->close();
 }
 ?>
+
+
+
+
+*/
